59d7d0dbba7c15cbf185712dbc12880504bb9481
Rolando Montas <roland@rolandos-macbook-pro.local>
2018-11-16 16:36:35 -0600

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Rolando Montas on 11/16/18.
+//  Copyright © 2018 Rolando Montas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
